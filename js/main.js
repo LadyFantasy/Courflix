0f8dbb97c5ba71d995430aedeeca169206c02ade
LadyFantasy <msoledadm88@gmail.com>
2020-05-28 13:47:44 +0100

    carrousel bb

--- js/main.js
+++ js/main.js
@@ -3,36 +3,50 @@ const navLinks = document.querySelector(".nav-links")
 
 
 menu.addEventListener("click", () => {
-  if(menu.style.display = "inline-block"){
-  
+  if (menu.style.display = "inline-block") {
+
     menu.classList.toggle("is-active")
     if (navLinks.classList.contains("open")) {
-      navLinks.classList.remove("open")}
-    
-    else{
+      navLinks.classList.remove("open")
+    } else {
       navLinks.classList.add("open")
     }
   }
-    })
+})
 
 
 
-$(document).ready(function(){
-$(".series-carrousel").slick({
+$(document).ready(function () {
+  $(".series-carrousel").slick({
     infinite: true,
     slidesToScroll: 2,
     variableWidth: true,
     accesibility: true,
     arrows: true,
-   responsive: [
-          {
-            breakpoint: 850,
-            settings: {
-              slidesToScroll: 1,
-            }
-          }
-    //    
-      ]
+    responsive: [{
+      breakpoint: 850,
+      settings: {
+        slidesToScroll: 1,
+      }
+    }]
+  })
+});
+
+
+
+$(document).ready(function () {
+  $(".series-carrousel-bb").slick({
+    infinite: true,
+    slidesToScroll: 2,
+    variableWidth: true,
+    accesibility: true,
+    arrows: true,
+    responsive: [
+    {
+      breakpoint: 600,
+      settings: "unslick"
+    },
+   ]
   })
 });
 
